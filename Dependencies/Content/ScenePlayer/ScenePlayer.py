f9b37d64d611b78f2d39bbb9b7fe5410523c23c0
janfiess <janfiess@users.noreply.github.com>
2025-07-28 18:12:49 +0200

    Umbau für AUdio

--- Dependencies/Content/ScenePlayer/ScenePlayer.py
+++ Dependencies/Content/ScenePlayer/ScenePlayer.py
@@ -3,12 +3,7 @@ import TDFunctions as TDF
 
 class ScenePlayer:
 	def __init__(self, ownerComp):
-		self.scenes = op.Footage.findChildren(name = "footage*", parName = "Mqttpayload", maxDepth=1)
-		# self.nextscene = op.ScenePlayer.par.Currentreplicatorinstance
-		# "accepted_payloads" is a list of expected payloads coming from the content.jsons when a mqtt msg with the topic "[mqtt_topic_prefix]play" arrives
-		self.accepted_payloads_list = []
-		self.Update_list_of_accepted_payloads()
-		self.moviefile = op.Footage.op(f"{op.ScenePlayer.par.Currentreplicatorinstance}").op("videofile")
+		return
 
 	# triggered by op.MQTT_in.op('trigger_by_incoming_mqtt')
 	def PlayScene(self, payload):
@@ -37,94 +32,47 @@ class ScenePlayer:
 			
 
 
-	def Play_new(self):
-		#print("start play new")
-		parent().par.Prevreplicatorinstance = parent().par.Currentreplicatorinstance
-		parent().par.Currentreplicatorinstance = parent().par.Nextreplicatorinstance  # used to select the right scene
-		if str(parent().par.Currentreplicatorinstance) == "":
-			parent().par.Currentreplicatorinstance = op.ScenePlayer.par.Prevreplicatorinstance
-		parent().par.Nextreplicatorinstance  = None
-
-		self.moviefile = op.Footage.op(f"{op.ScenePlayer.par.Currentreplicatorinstance}").op("videofile")
+	def Play(self, playername):
+		print("playing")
+		player = op(playername)
+		player.par.play = 1
 	
-		if (absTime.seconds - op.ScenePlayer.par.Latestscenemqtttime) > 1:
-			self.moviefile.par.cuepoint = 0
-			self.moviefile.par.cuepulse.pulse()
+		player.par.cuepoint = 0
+		player.par.cuepulse.pulse()
 
-		# self.moviefile.par.play = 1
-		parent().par.Pause = 0
-		op.Logger.Info(f"[ScenePlayer]: playing now {parent().par.Latestscenemqttpayload}")
+		op.Logger.Info(f"[ScenePlayer]: playing now {playername}")
 
-		
-	def GetSceneName(self, payload):
-		scene = None
-		for sc in self.scenes:
-			scene_mqtt_payload_from_cms = op(sc.path).par.Mqttpayload
-
-			if str(scene_mqtt_payload_from_cms) in str(payload):
-				scene = sc
-				op.ScenePlayer.par.Sceneindex = int(op(sc.path).name[7:])
-				break
-		# print(f"[ScenePlayer]: GetSceneName: payload: {payload}, scene.name: {scene.name}")
-		return scene.name
-	
-
-	# pause video
-	def Pause(self):
-		self.moviefile.par.play = 0
-		parent().par.Pause = 1
-		op.Timer.par.Pausepressed.pulse()
+	def Pause(self, playername):
+		print("pause")
+		player = op(playername)
+		player.par.play = 0
+		op.Logger.Info(f"[ScenePlayer]: pausing {playername}")
 
 
-	# triggered from topic:"[topicprefix]player" payload: "continue")
-	def Continue(self):
-		self.moviefile.par.play = 1
-		parent().par.Pause = 0
-		op.Timer.par.Resetalltimers.pulse()
-		op.Logger.Info(f"[ScenePlayer]: Continue {self.moviefile.name}")
 
-	def Restart(self):
-		self.moviefile.par.cuepulse.pulse()
 
 	def OnParamChanged(self, par):
-		if par.name == "Pause":
+		if par.name == "Audio1de":
 			if par == 1:
-				self.Pause()
-				op.Logger.Info(f"[ScenePlayer]: Pause video")
+				self.Play("Audio1_DE")
 			elif par == 0:
-				self.Continue()
-				op.Logger.Info(f"[ScenePlayer]: Continue video now")
-			
-		elif par.name == "Restart":
-			self.Restart()
-			op.Logger.Info(f"[ScenePlayer]: restart video now")
+				self.Pause("Audio1_DE")
 
-		elif par.name == "Updateexpectedpayloads":
-			self.Update_list_of_accepted_payloads()
-			op.Logger.Info(f"[ScenePlayer]: updated list of accepted payloads")
+		elif par.name == "Audio1en":
+			if par == 1:
+				self.Play("Audio1_EN")
+			elif par == 0:
+				self.Pause("Audio1_EN")
 
+		elif par.name == "Audio2de":
+			if par == 1:
+				self.Play("Audio2_DE")
+			elif par == 0:
+				self.Pause("Audio2_DE")
 
-		elif par.name == "Fadeintime":	
-			par = round(float(par),1)
-			op.Logger.Info(f"[ScenePlayer]: Changed fadein time to {par}s")
-			# send status only via mqtt if it didn't come from the mqtt broker
-			topic = op.MQTT_in.par.Topicprefix  + "player/fadeintime"
-			if op.MQTT_in.par.Latestmqtttopic != topic  or (absTime.seconds - op.MQTT_in.par.Latestmqtttime) > 1: # nicht mehrmals hintereinander mqtt senden und empfangen
-				op.MQTT_out.SendMQTT_retained(topic, par)
-
-		elif par.name == "Fadeouttime":	
-			par = round(float(par),1)		
-			op.Logger.Info(f"[ScenePlayer]: Changed fadeout time to {par}s")
-			# send status only via mqtt if it didn't come from the mqtt broker
-			topic = op.MQTT_in.par.Topicprefix  + "player/fadeouttime"
-			if op.MQTT_in.par.Latestmqtttopic != topic  or (absTime.seconds - op.MQTT_in.par.Latestmqtttime) > 1: # nicht mehrmals hintereinander mqtt senden und empfangen
-				op.MQTT_out.SendMQTT_retained(topic, par)
-			
-	def Update_list_of_accepted_payloads(self):
-		accepted_payloads_table = op.ScenePlayer.op("null_expected_videos_from_cms")
-		self.accepted_payloads_list = []
-
-		for row in range(1, accepted_payloads_table.numRows):
-			self.accepted_payloads_list.append(str(accepted_payloads_table[row, 0]))
-
-		# op.Logger.Info(f"[ScenePlayer]: Accepted_payloads_list: {self.accepted_payloads_list}")
+		elif par.name == "Audio2en":
+			if par == 1:
+				self.Play("Audio2_EN")
+			elif par == 0:
+				self.Pause("Audio2_EN")
+				
